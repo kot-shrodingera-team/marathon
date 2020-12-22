66bece7483681dabd42ae5777f3fc419fa7cd99b
kot-shrodingera <kot.shrodingera@gmail.com>
2020-12-23 01:48:31 +0600

    fix: селекторы инпутов логина, пароля, селектор блока авторизации

--- src/stake_info/checkAuth.ts
+++ src/stake_info/checkAuth.ts
@@ -3,7 +3,7 @@ import checkAuthGenerator, {
 } from '@kot-shrodingera-team/germes-generators/stake_info/checkAuth';
 
 export const authStateReady = authStateReadyGenerator({
-  noAuthElementSelector: '#auth_block',
+  noAuthElementSelector: '.auth-block',
   authElementSelector: '.profile__icon',
   // maxDelayAfterNoAuthElementAppeared: 0,
   logging: true,
